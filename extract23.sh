e46f483e9bf30803e956feed88d4cac39b137b97
teepean <tnatkinn@gmail.com>
2019-06-04 22:30:19 +0300

    Update extract23.sh to handle both hg19 and GRCh37 using hg19 as a reference fasta
    Update extract23.sh to handle both hg19 and GRCh37 using hg19 as a reference fasta

--- extract23.sh
+++ extract23.sh
@@ -77,7 +77,7 @@ fi
 # The -l parameter requires mpileup to exactly pick the constellations at the SNP positions
 # without having to screen through the whole genome base by base.
 # samtools mpileup -R -B -q30 -Q30 -v -l ${REF_23ANDME} -f ${REF} ${BAMFILE_SORTED} > 23andMe_raw.vcf.gz
-samtools view -H ${BAMFILE_SORTED} | /bin/sed -e 's/SN:\([0-9XY]\)/SN:chr\1/' -e 's/SN:MT/SN:chrM/' | /bin/samtools reheader - ${BAMFILE_SORTED} > bam_tmp.bam
+samtools view -H ${BAMFILE_SORTED} | /bin/sed -e 's/SN:\([0-9XY]\)/SN:chr\1/' -e 's/SN:MT/SN:chrM/' | samtools reheader - ${BAMFILE_SORTED} > bam_tmp.bam
 samtools mpileup -C 50 -v -l ${REF_23ANDME} -f ${REF} bam_tmp.bam > 23andMe_raw.vcf.gz
 tabix -p vcf 23andMe_raw.vcf.gz
 
